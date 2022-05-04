f8fe8f66b3508721b8d51b11641fd14331f400cc
Martin <martin_lenart@icloud.com>
2022-05-04 12:18:03 +0200

    Update

--- ValueRef/Program.cs
+++ ValueRef/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace ValueRef
 {
@@ -16,45 +15,42 @@ namespace ValueRef
 
         static void Main(string[] args)
         {
+            /*
+            var Ac = new Aclass();
+
+            int a = 10;
+            int b;
+            DoSomething0(a, out b);
+            Console.WriteLine($"a:{a}, b:{b}");
+
+            a = 10;
+            As.MyInt = 10;
+            DoSomething3(a, As);
+            Console.WriteLine($"a:{a}, As.MyInt:{As.MyInt}");
+            
+
+            int i = 0;
+            bool b1 = true;
+            float f = 0F;
+            double d = 0D;
+
+            double myPi;
+            SetValues(i, b1, f, d, out myPi);
+            SetValues(0, true, 10.10F, 10.10D, out double myPi2);
+
+            Console.WriteLine($"i:{i}, b1:{b1}, f:{f}, d:{d}, myPi: {myPi}, myPi2:{myPi2}");
+            */
+
             var As = new Astruct { MyInt=4 };
             var Ac = new Aclass { MyInt = 4 };
             int a = 4;
             int[] myArray = { 1, 2, 3, 4 };
-
             DoSomething1(a, myArray, As, Ac);
             Console.WriteLine($"a:{a}, myArray[3]:{myArray[3]}, As.MyInt:{As.MyInt}, Ac.MyInt:{Ac.MyInt}");
-            //a:4, myArray[3]:5, As.MyInt:4, Ac.MyInt:5
 
-
-            DoSomething1a(a, myArray, As, Ac,
-                out int b1, out int[] b1array, out Astruct b1struct, out Aclass b1class);
-            Console.WriteLine($"\na:{a}, myArray[3]:{myArray[3]}, As.MyInt:{As.MyInt}, Ac.MyInt:{Ac.MyInt}, " +
-                $"b1:{b1}, b1array[3]:{b1array[3]}, b1struct.MyInt:{b1struct.MyInt}, b1class.MyInt:{b1class.MyInt}");
-            //a:4, myArray[3]:5, As.MyInt:4, Ac.MyInt:5, b1:10, b1array[3]:40, b1struct.MyInt:10, b1class.MyInt:10
-
-
-            int[] iArray1 = { 1, 2, 3, 4 };
-            int[] iArray2 = { 9, 8, 7, 6 };
-            DoSomething1b(iArray1, iArray2);
-            Console.WriteLine($"\niArray1[0]:{iArray1[0]}, iArray2[0]:{iArray2[0]}");
-            //iArray1[0]:10, iArray2[0]:9
-
-            int i1 = 4;
-            int i2 = 4;
-            DoSomething1c(i1, ref i2);
-            Console.WriteLine($"\ni1:{i1}, i2:{i2}");
-            //i1:4, i2:2147483647
-
-            int[] iArray3 = { 1, 2, 3, 4 };
-            int[] iArray4 = { 9, 8, 7, 6 };
-            DoSomething1d(iArray3, ref iArray4);
-            Console.WriteLine($"\niArray3[0]:{iArray3[0]}, iArray4[0]:{iArray4[0]}");
-            //iArray3[0]:1, iArray4[0]:10
         }
 
-        //values of all value types (int, struct) will be copied into the params
-        //References of all reference types (array, class) will be copied
-        //Only the objects on the heap can be changed from the methos to the caller
+        //Some Methods to test
         static void DoSomething1(int a, int[] array, Astruct astruct, Aclass aclass)
         {
             a = 5;
@@ -64,46 +60,55 @@ namespace ValueRef
         }
 
 
-        //I can pass any type as an out parameter. Either value or reference is passed
-        static void DoSomething1a(int a, int[] array, Astruct astruct, Aclass aclass,
-                                  out int b, out int[] barray, out Astruct bstruct, out Aclass bclass)
-        {
-            a = 5;
-            array[3] = 5;
-            astruct.MyInt = 5;
-            aclass.MyInt = 5;
 
-            b = 10;
-            barray = new int[] { 10, 20, 30, 40 };
-            bstruct = new Astruct { MyInt = 10 };
-            bclass = new Aclass { MyInt = 10 };
+
+
+
+
+/*
+        public static void SetValues(int i, bool b, float f, double d, out double pi)
+        {
+            i = 10;
+            b = false;
+            f = 10.10F;
+            d = 10.10D;
+            Console.WriteLine($"i:{i}, b:{b}, f:{f}, d:{d}");
+            pi = Math.PI;
+
+
+        }
+        static void DoSomething0(int in1, out int z1)
+        {
+            in1 = 100;
+            z1 = 100;
+        }
+        static void DoSomething3(int a, Astruct myA)
+        {
+            a = 100;
+            myA.MyInt = 100;
         }
 
-        //passing and changing reference types
-        static void DoSomething1b(int[] array1, int[] array2)
-        {
-            array1[0] = 10;         //array1 is referenceing same object on the heap as the caller
-                                    //Hence array from caller is changed
 
-            array2 = new int[] { 10, 20, 30, 40, 50 };  //array2 is now referencing a new object on the heap.
-                                                        //thrown away when leaving scope
-                                                        //Hence caller array not modfied 
-        }
-
-        //passing value types and ref to value types
-        static void DoSomething1c (int a1, ref int a2)
+        static void DoSomething1(int a, out Aclass outB)
         {
-            a1 = int.MaxValue;      //Value type. trown away when leaving scope
-            a2 = int.MaxValue;      //Ref of a value type - refers to original variable of caller
-                                    //Hence caller variable  changes
+            outB = new Aclass { MyInt = a * 2 };
+            a = a * 10;
         }
-
-        //passing reference types and ref to reference types
-        static void DoSomething1d(int[] array1, ref int[] array2)
+        
+        static void DoSomething2(int a, int[] array)
         {
-            array1 = new int[] { 10, 20, 30, 40, 50};  //New instance referred to, will be thrown away when leaving scope
-            array2 = new int[] { 10, 20, 30, 40, 50};  //Ref of refernce type - refers to original variable of caller
-                                                       //Hence caller variable changes
+            array = new int[25];
+            array[3] = a;
         }
+         static void DoSomething4(int a, Aclass myB)
+        {
+             myB.MyInt = a;
+        }
+        static void DoSomething5(int a, Aclass myB)
+        {
+            myB = new Aclass();
+            myB.MyInt = a;
+        }
+        */
     }
 }
